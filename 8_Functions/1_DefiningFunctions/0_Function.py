43d42d0d3ec5e18be6e5cefaeef1884686108a25
marcos <marco.quichimbo@epn.edu.ec>
2023-08-30 08:09:34 -0500

    1_DefiningFunctions

--- 8_Functions/1_DefiningFunctions/0_Function.py
+++ 8_Functions/1_DefiningFunctions/0_Function.py
@@ -1,3 +1,5 @@
 def greet_user():
+    """Display a simple greeting
+    """
     print("Hello!")
 greet_user()
---
+++ 8_Functions/1_DefiningFunctions/2_ArgumentsParameters.py
@@ -0,0 +1,4 @@
+# in the definition of the function greet_user(username)
+# username == parameter 
+# in the call of the function greet_user("marcos")
+# marcos == argument 
