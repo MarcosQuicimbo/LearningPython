9cdd063b458580f0303ab193cfda884e4afdd541
marcos <marco.quichimbo@epn.edu.ec>
2023-08-30 08:06:31 -0500

    1_DefiningAFunction

---
+++ 8_Functions/1_DefiningFunctions/3_TryItYourSelf.py
@@ -0,0 +1,11 @@
+print("8.1 Message")
+def display_message():
+    print("I am learning about functions in python")
+    print("parameters, in the definition of hte function")
+    print("arguments, in the call of the function")
+display_message()
+
+print("8.2 favorite book")
+def favorite_book(title):
+    print("One of my favorite books is ", title)
+favorite_book("Inferno")
