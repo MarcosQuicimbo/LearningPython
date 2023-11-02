e89c4dbaa7fa8f50cef03c09962c9e5ba352723e
marcos <marco.quichimbo@epn.edu.ec>
2023-11-02 13:23:55 -0500

    2_WrittingToAFile

---
+++ 10_FilesAndExceptions/2_WrittingToAFile/AppendingToAFile.py
@@ -0,0 +1,7 @@
+file_name = 'programming_2.txt'
+
+with open(file_name, 'a') as file_object:
+    file_object.write('Uno \n')
+    file_object.write('Dos \n')
+    file_object.write('Tress \n')
+    file_object.write('Cuatro \n')
---
+++ 10_FilesAndExceptions/2_WrittingToAFile/WritingMultiplesLines.py
@@ -0,0 +1,7 @@
+file_name = 'programming_2.txt'
+
+with open(file_name,'w') as file_object:
+    file_object.write('Yo amo a la mimosa \n')
+    file_object.write('La mimosa me ama mucho \n')
+
+    
